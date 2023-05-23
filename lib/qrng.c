cbcdf748407f4e5e7a2eb745b9f4a16bacf61e00
Sebastian Ardelean <sebastian.ardelean@cs.upt.ro>
2023-05-23 16:57:02 +0300

    refactored library

--- lib/qrng.c
+++ lib/qrng.c
@@ -35,6 +35,7 @@ typedef struct
 {
   e_req_type_t type;
   const char *api_url;
+  char ip_address[IPV4_IP_LENGTH];
   size_t samples;
   int64_t min_range_i;
   int64_t max_range_i;
@@ -48,19 +49,91 @@ typedef struct {
 }memory_t;
 
 
-static const s_api_t api_types[]={
-  {SHORT_RANDOM_NUMBER,  ""                                                      , DEFAULT_NUMBER_OF_SAMPLES, MIN_VALUE_INT, MAX_VALUE_INT, MIN_VALUE_FLOAT, MAX_VALUE_FLOAT},
-  {INT_RANDOM_NUMBER,    "https://%s/api/2.0/int?min=%lu&max=%lu&quantity=%lu"   , DEFAULT_NUMBER_OF_SAMPLES, MIN_VALUE_INT, MAX_VALUE_INT, MIN_VALUE_FLOAT, MAX_VALUE_FLOAT},
-  {DOUBLE_RANDOM_NUMBER, "https://%s/api/2.0/double?min=%lf&max=%lf&quantity=%lu", DEFAULT_NUMBER_OF_SAMPLES, MIN_VALUE_INT, MAX_VALUE_INT, MIN_VALUE_FLOAT, MAX_VALUE_FLOAT},
-  {FLOAT_RANDOM_NUMBER,  ""                                                      , DEFAULT_NUMBER_OF_SAMPLES, MIN_VALUE_INT, MAX_VALUE_INT, MIN_VALUE_FLOAT, MAX_VALUE_FLOAT},
-  {STREAM_BINARY,        "https://%s/api/2.0/streambytes?size=%lu"               , DEFAULT_NUMBER_OF_SAMPLES, MIN_VALUE_INT, MAX_VALUE_INT, MIN_VALUE_FLOAT, MAX_VALUE_FLOAT}, 
-  {PERFORMANCE_REQUEST,  ""                                                      , DEFAULT_NUMBER_OF_SAMPLES, MIN_VALUE_INT, MAX_VALUE_INT, MIN_VALUE_FLOAT, MAX_VALUE_FLOAT},
-  {FIRMWARE_INFO_REQUEST,""                                                      , DEFAULT_NUMBER_OF_SAMPLES, MIN_VALUE_INT, MAX_VALUE_INT, MIN_VALUE_FLOAT, MAX_VALUE_FLOAT},
-  {SYSTEM_INFO_REQUEST,  ""                                                      , DEFAULT_NUMBER_OF_SAMPLES, MIN_VALUE_INT, MAX_VALUE_INT, MIN_VALUE_FLOAT, MAX_VALUE_FLOAT}
+static s_api_t api_types[]={
+  {
+    .type = SHORT_RANDOM_NUMBER,
+    .api_url = "",
+    .ip_address = "",
+    .samples = DEFAULT_NUMBER_OF_SAMPLES,
+    .min_range_i = MIN_VALUE_INT,
+    .max_range_i = MAX_VALUE_INT,
+    .min_range_f = MIN_VALUE_FLOAT,
+    .max_range_f = MAX_VALUE_FLOAT
+  },
+  {
+    .type = INT_RANDOM_NUMBER,
+    .api_url = "https://%s/api/2.0/int?min=%lu&max=%lu&quantity=%lu",
+    .ip_address = "",
+    .samples = DEFAULT_NUMBER_OF_SAMPLES,
+    .min_range_i = MIN_VALUE_INT,
+    .max_range_i = MAX_VALUE_INT,
+    .min_range_f = MIN_VALUE_FLOAT,
+    .max_range_f = MAX_VALUE_FLOAT
+  },
+  {
+    .type = DOUBLE_RANDOM_NUMBER,
+    .api_url = "https://%s/api/2.0/double?min=%lf&max=%lf&quantity=%lu",
+    .ip_address = "",
+    .samples = DEFAULT_NUMBER_OF_SAMPLES,
+    .min_range_i = MIN_VALUE_INT,
+    .max_range_i = MAX_VALUE_INT,
+    .min_range_f = MIN_VALUE_FLOAT,
+    .max_range_f = MAX_VALUE_FLOAT
+  },
+  {
+    .type = FLOAT_RANDOM_NUMBER,
+    .api_url = "",
+    .ip_address = "",
+    .samples = DEFAULT_NUMBER_OF_SAMPLES,
+    .min_range_i = MIN_VALUE_INT,
+    .max_range_i = MAX_VALUE_INT,
+    .min_range_f = MIN_VALUE_FLOAT,
+    .max_range_f = MAX_VALUE_FLOAT
+  },
+  {
+    .type = STREAM_BINARY,
+    .api_url = "https://%s/api/2.0/streambytes?size=%lu",
+    .ip_address = "",
+    .samples = DEFAULT_NUMBER_OF_SAMPLES,
+    .min_range_i = MIN_VALUE_INT,
+    .max_range_i = MAX_VALUE_INT,
+    .min_range_f = MIN_VALUE_FLOAT,
+    .max_range_f = MAX_VALUE_FLOAT
+  }, 
+  {
+    .type = PERFORMANCE_REQUEST,
+    .api_url = "",
+    .ip_address = "",
+    .samples = DEFAULT_NUMBER_OF_SAMPLES,
+    .min_range_i = MIN_VALUE_INT,
+    .max_range_i = MAX_VALUE_INT,
+    .min_range_f = MIN_VALUE_FLOAT,
+    .max_range_f = MAX_VALUE_FLOAT
+  },
+  {
+    .type = FIRMWARE_INFO_REQUEST,
+    .api_url = "",
+    .ip_address = "",
+    .samples = DEFAULT_NUMBER_OF_SAMPLES,
+    .min_range_i = MIN_VALUE_INT,
+    .max_range_i = MAX_VALUE_INT,
+    .min_range_f = MIN_VALUE_FLOAT,
+    .max_range_f = MAX_VALUE_FLOAT
+  },
+  {
+    .type = SYSTEM_INFO_REQUEST,
+    .api_url = "",
+    .ip_address = "",
+    .samples = DEFAULT_NUMBER_OF_SAMPLES,
+    .min_range_i = MIN_VALUE_INT,
+    .max_range_i = MAX_VALUE_INT,
+    .min_range_f = MIN_VALUE_FLOAT,
+    .max_range_f = MAX_VALUE_FLOAT
+  }
 };
 
 
-static char ip_address[IPV4_IP_LENGTH] = {0};
+
 
 static CURL *p_curl_handle = NULL;
 
@@ -68,42 +141,49 @@ static size_t curl_write_cbk(void *content,
 		      size_t size, 
 		      size_t nmemb, 
 		      void *userp);
-static int create_req_url(e_req_type_t req_type, char *api_url);
+static void create_req_url(e_req_type_t req_type, char *api_url);
 static int execute_request();
 
 
 int qrng_open(const char *device_ip_address){
 
     int retval = 0;
+    size_t i = 0;
 
-    strncpy(ip_address, device_ip_address, IPV4_IP_LENGTH);
-     
+    if (device_ip_address[0] != '\0') {
+      for (i = 0 ; i < NUMBER_OF_REQUESTS; i++) {
+        strncpy(api_types[i].ip_address, device_ip_address, IPV4_IP_LENGTH);
+      }
 
-    if(curl_global_init(CURL_GLOBAL_ALL)!=0) {
+      
+      if(curl_global_init(CURL_GLOBAL_ALL)!=0) {
 	fprintf(stderr, "Error in curl_global_init");
 	retval = -1;
 	
-    }
-    else {
+      }
+      else {
 	p_curl_handle = curl_easy_init();
 	if (!p_curl_handle) {
-	    fprintf(stderr, "Error in curl_easy_init");
-            curl_global_cleanup();
-	    retval = -2;
-	} else {
+          fprintf(stderr, "Error in curl_easy_init");
+          curl_global_cleanup();
+          retval = -2;
+	}
+        else {
 
 #ifdef DEBUG
-	    (void)curl_easy_setopt(p_curl_handle, CURLOPT_VERBOSE, 1L);
+          (void)curl_easy_setopt(p_curl_handle, CURLOPT_VERBOSE, 1L);
 #endif
-	    (void)curl_easy_setopt(p_curl_handle, CURLOPT_SSL_VERIFYHOST, 0L);
+          (void)curl_easy_setopt(p_curl_handle, CURLOPT_SSL_VERIFYHOST, 0L);
 
-	    (void)curl_easy_setopt(p_curl_handle, CURLOPT_SSL_VERIFYPEER, 0L);
-
+          (void)curl_easy_setopt(p_curl_handle, CURLOPT_SSL_VERIFYPEER, 0L);
 	    	    
-	    (void)curl_easy_setopt(p_curl_handle, CURLOPT_FAILONERROR, 1L);
+          (void)curl_easy_setopt(p_curl_handle, CURLOPT_FAILONERROR, 1L);
 	}
+      }
     }
-
+    else {
+      retval = -3;
+    }
     return retval;
  
 }
@@ -137,14 +217,12 @@ int qrng_random_float(float min, float max, size_t samples, float *buffer)
 
 int qrng_random_int64(int64_t min, int64_t max, size_t samples, int64_t *buffer)
 {
-
+  return 0;
 }
 
 int qrng_random_int32(int32_t min, int32_t max, size_t samples, int32_t *buffer)
 {
     int retval = 0;
-
-    CURLcode error = CURLE_OK;    
 
     char final_url[256]={0};
 
@@ -155,31 +233,27 @@ int qrng_random_int32(int32_t min, int32_t max, size_t samples, int32_t *buffer)
     if (mem_buffer.memory) {
       memset(&mem_buffer, 0, sizeof(mem_buffer));
         
-      if(!(retval=create_req_url(SHORT_RANDOM_NUMBER, final_url))) {
+      create_req_url(SHORT_RANDOM_NUMBER, final_url);
       
-        retval = execute_request(final_url, (void *)(&mem_buffer));
+      retval = execute_request(final_url, (void *)(&mem_buffer));
       
-        if (!retval) {
-          //parse values array
-          char *random_values_string = mem_buffer.memory;
-          /* Skip first character because it's [ */
-          random_values_string ++;
-          /* Skip last character because is ] */
-          random_values_string[strlen(random_values_string)-1]=0;
-          char *token = strtok(random_values_string,",");
-          size_t i = 0;
-          
-          for (i = 0; i < samples; i++) {
-            buffer[i] = atol(token);
-            token = strtok(NULL, ",");
-          }   
-        }
-        else {
-          fprintf(stderr, "could not execute curl request");
-        }
+      if (!retval) {
+        /* parse values array */
+        char *random_values_string = mem_buffer.memory;
+        /* Skip first character because it's [ */
+        random_values_string ++;
+        /* Skip last character because is ] */
+        random_values_string[strlen(random_values_string)-1]=0;
+        char *token = strtok(random_values_string,",");
+        size_t i = 0;
+        
+        for (i = 0; i < samples; i++) {
+          buffer[i] = atol(token);
+          token = strtok(NULL, ",");
+        }   
       }
-      else{
-        fprintf(stderr, "reqeuest url could not be created");
+      else {
+        fprintf(stderr, "could not execute curl request");
       }
     }
     else {
@@ -220,47 +294,41 @@ int execute_request(char *url, void *buffer) {
 
 
 
-int create_req_url(e_req_type_t req_type, char *api_url)
+void create_req_url(e_req_type_t req_type, char *api_url)
 {
-  int retval = 0;
 
-  if (ip_address[0] != '\0') {
-    retval = -1;
+  switch(req_type) {
+  case SHORT_RANDOM_NUMBER:
+  case INT_RANDOM_NUMBER:
+    snprintf(api_url, URL_MAX_LENGTH, api_types[req_type].api_url,
+             api_types[req_type].ip_address,
+             api_types[req_type].min_range_i,
+             api_types[req_type].max_range_i,
+             api_types[req_type].samples);
+    break;
+  case DOUBLE_RANDOM_NUMBER:
+  case FLOAT_RANDOM_NUMBER:
+    snprintf(api_url, URL_MAX_LENGTH, api_types[req_type].api_url,
+             api_types[req_type].ip_address,
+             api_types[req_type].min_range_f,
+             api_types[req_type].max_range_f,
+             api_types[req_type].samples);
+    break;
+  case STREAM_BINARY:
+    snprintf(api_url, URL_MAX_LENGTH, api_types[req_type].api_url,
+             api_types[req_type].ip_address,
+             api_types[req_type].samples);
+    break;
+  case PERFORMANCE_REQUEST:
+    break;
+  case FIRMWARE_INFO_REQUEST:
+    break;
+  case SYSTEM_INFO_REQUEST:
+    break;
+  default:
+    break;
   }
-  else {
-    switch(req_type) {
-    case SHORT_RANDOM_NUMBER:
-    case INT_RANDOM_NUMBER:
-      snprintf(api_url, URL_MAX_LENGTH, api_types[req_type].api_url,
-               ip_address,
-               api_types[req_type].min_range_i,
-               api_types[req_type].max_range_i,
-               api_types[req_type].samples);
-      break;
-    case DOUBLE_RANDOM_NUMBER:
-    case FLOAT_RANDOM_NUMBER:
-      snprintf(api_url, URL_MAX_LENGTH, api_types[req_type].api_url,
-               ip_address,
-               api_types[req_type].min_range_f,
-               api_types[req_type].max_range_f,
-               api_types[req_type].samples);
-      break;
-    case STREAM_BINARY:
-      snprintf(api_url, URL_MAX_LENGTH, api_types[req_type].api_url,
-               ip_address,
-               api_types[req_type].samples);
-      break;
-    case PERFORMANCE_REQUEST:
-      break;
-    case FIRMWARE_INFO_REQUEST:
-      break;
-    case SYSTEM_INFO_REQUEST:
-      break;
-    default:
-      break;
-    }
-  }
-  return retval;
+
 }
 
 
@@ -279,3 +347,4 @@ size_t curl_write_cbk(void *content, size_t size, size_t nmemb, void *userp)
     mem->size += real_size;
     mem->memory[mem->size] = 0;
     return real_size;
+}
